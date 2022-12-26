72706d8516c07c3b6877bee9526c48d4e16fe1fd
Kevin Lago <kevinthelago@gmail.com>
2022-12-26 15:16:06 -0500

    Java Map

--- src/data_structures/java_map/Solution.java
+++ src/data_structures/java_map/Solution.java
@@ -1,5 +1,7 @@
 package data_structures.java_map;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
@@ -8,13 +10,20 @@ public class Solution {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         in.nextLine();
+        Map<String, Integer> phoneBook = new HashMap<>();
+
         for (int i = 0; i < n; i++) {
             String name = in.nextLine();
             int phone = in.nextInt();
             in.nextLine();
+
+            phoneBook.put(name, phone);
         }
+
         while (in.hasNext()) {
             String s = in.nextLine();
+
+            System.out.println(phoneBook.get(s) != null ? s + "=" + phoneBook.get(s) : "Not found");
         }
     }
 
